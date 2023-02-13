ff0b4e2f8b09eb10dd004dbd70f85dac703fe274
ideen1 <ibanijamali@gmail.com>
2023-02-12 21:23:20 -0700

    Add Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-5GES0RQTCG');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
