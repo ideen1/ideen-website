738be5242534774833f349f82c3912d5eade384b
ideen1 <ibanijamali@gmail.com>
2025-01-21 01:00:31 -0700

    Fix Terminal Width

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -36,38 +36,38 @@ export default function AboutCard() {
 
   return (
     <>
-      <div className="grid m-auto bg-zinc-700 h-fit md:h-1/2 w-full lg:w-3/5 rounded-3xl">
-        <Terminal name="Ideen" colorMode={ColorMode.Dark} height="100%" >
-          <div className="grid grid-cols-1 h-full md:grid-cols-2 place-items-center">
-            <div className="grid h-full w-full md:border-r-2 content-center">
+      <div className="grid w-full m-auto bg-zinc-700 h-fit md:h-1/2 xl:w-3/5 rounded-3xl">
+        <Terminal name="Ideen" colorMode={ColorMode.Dark} height="100%">
+          <div className="grid h-full grid-cols-1 md:grid-cols-2 place-items-center">
+            <div className="grid content-center w-full h-full md:border-r-2">
               <div className="grid m-5 place-items-center">
-                <div className="grid h-full w-4/5">
+                <div className="grid w-4/5 h-full">
                   <Image
-                    className="rounded-full m-auto"
+                    className="m-auto rounded-full"
                     src={"/me.jpg"}
                     alt={"Ideen Photo"}
                     width={150}
                     height={150}
                   />
-                  <h1 className="grid text-3xl font-bold text-white m-auto">
+                  <h1 className="grid m-auto text-3xl font-bold text-white">
                     Ideen Banijamali
                   </h1>
-                  <h2 className="grid text-2xl font-bold text-accent m-auto">
+                  <h2 className="grid m-auto text-2xl font-bold text-accent">
                     <TextScramble text={currentThing} scramble={8} />
                   </h2>
                 </div>
               </div>
             </div>
             <div className="grid h-4/5 place-content-center">
-              <div className="grid m-auto justify-self-start w-4/5">
+              <div className="grid w-4/5 m-auto justify-self-start">
                 <p className="text-white">
                   Welcome to ideen.ca! I am a Software Engineering student who
                   is passionate about creating innovative solutions. Through
                   previous internships and clubs, I have worked with various
                   programming languages and technologies. I am eager to continue
                   learning and taking on new challenges to make a positive
-                  impact through software development. I&apos;m currently working at <b>Garmin</b> as a Software
-                  Engineering Intern.
+                  impact through software development. I&apos;m currently
+                  working at <b>Garmin</b> as a Software Engineering Intern.
                 </p>
                 <div></div>
               </div>
